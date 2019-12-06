9bfdafa4c02dfedf78fde05080f3bf9f269ec655
Jussi Roberts <jussi.roberts@gmail.com>
2019-12-06 10:30:20 +0100

    Day4 P2

--- day4.py
+++ day4.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def day4():
     start = 254032
     end = 789860
@@ -6,12 +9,13 @@ def day4():
     for start in range(start, end):
         my_list = list(str(start))
         sorted_list = sorted(my_list)
-        if my_list == sorted_list:
-            my_set = set(my_list)
-            total += 1
-            if len(my_set) == len(my_list):
-                # print("found an invalid one")
-                total -= 1
+        c = Counter(sorted_list)
+        if 2 in c.values():
+            if my_list == sorted_list:
+                my_set = set(my_list)
+                total += 1
+                if len(my_set) == len(my_list):
+                    total -= 1
 
     print("Total number of password combinations the little elves are going to have try:", total, "- Poor sods.")
 
